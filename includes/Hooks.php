7a00dea0ca1a4807a6b72333caf793fdfe62e156
Adam Roses Wight <awight@wikimedia.org>
2015-10-17 03:10:30 -0700

    Flag reverted risk rows using the recentChangesFlag
    Also flips the filter logic to be correct--we want "Hide" to filter out any
    presumed "good" edits.
    
    Using the recentChangesFlag is simpler to maintain and is consistent with other
    uses of recent changes flags.
    
    Regresses slightly by not rolling up child "R" flags into the top-level grouped
    lines, but I'd prefer to implement that in a general way in mw-core rather than
    workaround here.  See @If9fd6af3ac.
    
    Bug: T112856
    Change-Id: Ic49a355a2a4a35d78dbd8dc720db2da4de2e628f

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -50,7 +50,7 @@ class Hooks {
 	 * @param $filters
 	 */
 	public static function onChangesListSpecialPageFilters( ChangesListSpecialPage $clsp, &$filters ) {
-		$filters['hidereverted'] = array(
+		$filters['hidenonreverted'] = array(
 			'msg' => 'ores-reverted-filter',
 			'default' => false,
 		);
@@ -73,12 +73,19 @@ class Hooks {
 		$name, array &$tables, array &$fields, array &$conds,
 		array &$query_options, array &$join_conds, FormOptions $opts
 	) {
-		if ( !$opts->getValue( 'hidereverted' ) ) {
-			$tables[] = 'ores_classification';
-			$fields[] = 'ores_probability';
-			$join_conds['ores_classification'] = array( 'LEFT JOIN',
-				'rc_this_oldid = ores_rev AND ores_model = \'reverted\' ' .
-				'AND ores_is_predicted = 1 AND ores_class = \'true\'' );
+		global $wgOresRevertFilterThreshold;
+
+		$tables[] = 'ores_classification';
+		$fields[] = 'ores_probability';
+		$join_conds['ores_classification'] = array( 'LEFT JOIN',
+			'rc_this_oldid = ores_rev AND ores_model = \'reverted\' ' .
+			'AND ores_is_predicted = 1 AND ores_class = \'true\'' );
+
+		if ( $opts->getValue( 'hidenonreverted' ) ) {
+			// Filter out non-damaging edits.
+			$conds[] = 'ores_is_predicted = 1';
+			$conds[] = 'ores_probability > '
+				. wfGetDb( DB_SLAVE )->addQuotes( $wgOresRevertFilterThreshold );
 		}
 
 		return true;
@@ -95,61 +102,38 @@ class Hooks {
 	public static function onEnhancedChangesListModifyLineData( EnhancedChangesList $ecl, array &$data,
 		array $block, RCCacheEntry $rcObj
 	) {
+		self::processRecentChangesList( $rcObj, $data );
+
+		return true;
+	}
+
+	/**
+	 * Label recent changes with ORES scores (for top-level ungrouped lines)
+	 *
+	 * @param EnhancedChangesList $ecl
+	 * @param array $data
+	 * @param RCCacheEntry $rcObj
+	 */
+	public static function onEnhancedChangesListModifyBlockLineData( EnhancedChangesList $ecl,
+		array &$data, RCCacheEntry $rcObj
+	) {
+		self::processRecentChangesList( $rcObj, $data );
+
+		return true;
+	}
+
+	/**
+	 * Internal helper to label matching rows
+	 */
+	protected static function processRecentChangesList( RCCacheEntry $rcObj, array &$data ) {
 		$score = $rcObj->getAttribute( 'ores_probability' );
 		if ( $score !== null ) {
-			$type = self::getRevertThreshold( $score );
+			$type = Scoring::getRevertThreshold( $score );
 
 			if ( $type ) {
-				$data[] = self::getScoreHtml( $type, $score );
-
-				$ecl->getOutput()->addModuleStyles( 'ext.ores.styles' );
+				$data['recentChangesFlags']['revertedRisk'] = true;
+				// TODO: Stash the details in HTML so they can be retrieved by JS?
 			}
 		}
-
-		return true;
-	}
-
-	// FIXME: Repeated code.
-	public static function onOldChangesListRecentChangesLine( OldChangesList &$ocl, &$html,
-		RecentChange $rc, array &$classes
-	) {
-		$score = $rc->getAttribute( 'ores_probability' );
-		if ( $score !== null ) {
-			$type = self::getRevertThreshold( $score );
-
-			if ( $type ) {
-				$html = $html . ' ' . self::getScoreHtml( $type, $score );
-
-				$ocl->getOutput()->addModuleStyles( 'ext.ores.styles' );
-			}
-		}
-
-		return true;
-	}
-
-	protected static function getRevertThreshold( $score ) {
-		global $wgOresRevertTagThresholds;
-
-		$score = floatval( $score );
-		$type = null;
-		// TODO: Need to ensure the thresholds are ordered.
-		foreach ( $wgOresRevertTagThresholds as $name => $value ) {
-			if ( $score >= $value ) {
-				$type = $name;
-			}
-		}
-		return $type;
-	}
-
-	protected static function getScoreHtml( $type, $score ) {
-		$cssClass = 'mw-ores-' . $type;
-		$rounded = intval( 100 * $score );
-		$msg = wfMessage( 'ores-reverted-' . $type )->numParams( $rounded )->escaped();
-		$html = Html::rawElement( 'span', array(
-			'title' => $msg,
-			'class' => array( $cssClass, 'mw-ores-score' ),
-		), $msg );
-
-		return $html;
 	}
 }
--- includes/Scoring.php
+++ includes/Scoring.php
@@ -45,6 +45,32 @@ class Scoring {
 		return $wireData;
 	}
 
+	/**
+	 * @return string|null Threshold exceeded, or null if none.
+	 *
+	 * TODO: Should be in a model-specific module.
+	 */
+	public static function getRevertThreshold( $score ) {
+		global $wgOresRevertTagThresholds;
+
+		$score = floatval( $score );
+		$type = null;
+
+		// Find the nearest threshold exceeded by $score.
+		$highestExceededThreshold = null;
+		foreach ( $wgOresRevertTagThresholds as $name => $threshold ) {
+			if ( $score >= $threshold
+				// Ignore threshold if it's further from $score.
+				&& ( $threshold > $highestExceededThreshold || $highestExceededThreshold === null )
+			) {
+				$type = $name;
+				$highestExceededThreshold = $threshold;
+			}
+		}
+		return $type;
+	}
+
+
 	public static function instance() {
 		return new self();
 	}
