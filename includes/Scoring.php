4fd9313fb88a5c0c1ff91b73ad87f1390d9dfc28
Amir Sarabadani <Ladsgroup@gmail.com>
2016-06-01 20:27:30 +0430

    Add extra_param to pass to the ORES service and use precaching
    Change-Id: I4be941b08bdff6639a3b852343abc6ef33112921

--- includes/Scoring.php
+++ includes/Scoring.php
@@ -7,19 +7,22 @@ class Scoring {
 	 * @param integer|array $revisions Single or multiple revisions
 	 * @param string|array|null $models Single or multiple model names.  If
 	 * left empty, all configured models are queries.
+	 * @param array $params extra params to be passed to ORES endpoint
 	 * @return array Results in the form returned by ORES
 	 * @throws \RuntimeException
 	 */
-	public function getScores( $revisions, $models = null ) {
+	public function getScores( $revisions, $models = null, array $extra_params = [] ) {
 		if ( !$models ) {
 			global $wgOresModels;
 			$models = array_keys( array_filter( $wgOresModels ) );
 		}
 
-		$wireData = Api::request( [
+		$params = [
 			'models' => implode( '|', (array) $models ),
 			'revids' => implode( '|', (array) $revisions ),
-		] );
+		];
+
+		$wireData = Api::request( array_merge( $params, $extra_params ) );
 		return $wireData;
 	}
 
