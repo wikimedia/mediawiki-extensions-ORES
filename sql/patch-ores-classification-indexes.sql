e439511909112c5fe9e11783aca24768c6e110e3
Amir Sarabadani <Ladsgroup@gmail.com>
2016-07-20 16:22:10 +0430

    Remove trailing space
    Change-Id: I72acf596539fa2d591e76f2ff6c02596a818cf7b

--- sql/patch-ores-classification-indexes.sql
+++ sql/patch-ores-classification-indexes.sql
@@ -1,6 +1,6 @@
 --
 -- patch-ores-classification-indexes.sql
 --
--- Drop redundant oresc_rev index in favor of orecs_winner 
+-- Drop redundant oresc_rev index in favor of orecs_winner
 
 DROP INDEX /*i*/oresc_rev ON /*_*/ores_classification;
