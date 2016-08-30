31a73f5e04cf04fab5a920963b19b8abd77bb38b
Amir Sarabadani <Ladsgroup@gmail.com>
2016-08-31 02:05:58 +0430

    Improve CheckModelVersions.php
    * upsert when updating the model. Using a replace causes incrementing
      the primary key every time.
    * Bypass varnish cache when getting new model versions. This is a
      temporary hack until ORES sends the right cache headers so it isn't
      cached in varnish (T144193).
    
    Bug: T144195
    Bug: T144196
    Change-Id: I5baad1adcf4aeab742cce4bca9bc4e58dd4d6fec

--- maintenance/CheckModelVersions.php
+++ maintenance/CheckModelVersions.php
@@ -23,22 +23,28 @@ class CheckModelVersions extends Maintenance {
 		$models = $this->getModels();
 
 		foreach ( $models as $name => $info ) {
-			\wfGetDB( DB_MASTER )->update( 'ores_model',
+			$dbw = \wfGetDB( DB_MASTER );
+			$dbw->update( 'ores_model',
 				[
 					'oresm_is_current' => 0,
 				],
 				[
 					'oresm_name' => $name,
-					'oresm_version != ' . \wfGetDB( DB_SLAVE )->addQuotes( $info['version'] ),
+					'oresm_version != ' . $dbw->addQuotes( $info['version'] ),
 				],
 				__METHOD__
 			);
 
-			\wfGetDB( DB_MASTER )->replace( 'ores_model',
+			$dbw->upsert( 'ores_model',
+				[
+					'oresm_name' => $name,
+					'oresm_version' => $dbw->addQuotes( $info['version'] ),
+					'oresm_is_current' => 1,
+				],
 				[ 'oresm_name', 'oresm_version' ],
 				[
 					'oresm_name' => $name,
-					'oresm_version' => $info['version'],
+					'oresm_version' => $dbw->addQuotes( $info['version'] ),
 					'oresm_is_current' => 1,
 				],
 				__METHOD__
@@ -52,7 +58,9 @@ class CheckModelVersions extends Maintenance {
 	 * Return a list of models available for this wiki.
 	 */
 	protected function getModels() {
-		$modelData = Api::request();
+		$timestamp = \wfTimestampNow();
+		// Bypass the varnish cache
+		$modelData = Api::request( [ $timestamp => true ] );
 		if ( empty( $modelData['models'] ) ) {
 			throw new \RuntimeException( 'Bad response from ORES when requesting models: '
 				. json_encode( $modelData ) );
