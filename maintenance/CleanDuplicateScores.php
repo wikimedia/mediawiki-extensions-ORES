3969429f1dbbab8497adb6b21d1ef3cdf290380f
Amir Sarabadani <Ladsgroup@gmail.com>
2016-10-05 18:54:39 +0330

    Fixup maintenance/CleanDuplicateScores.php
    Change-Id: Iadf3b002dade4aa3c7ef99c6c3ba7fa824decb5d

--- maintenance/CleanDuplicateScores.php
+++ maintenance/CleanDuplicateScores.php
@@ -22,22 +22,26 @@ class CleanDuplicateScores extends Maintenance {
 	public function execute() {
 		$dbr = \wfGetDB( DB_REPLICA );
 		$dbw = \wfGetDB( DB_MASTER );
+		$groupConcat = $dbr->buildGroupConcatField(
+			'|',
+			'ores_classification AS OC',
+			'ores_classification.oresc_id',
+			'OC.oresc_id = ores_classification.oresc_id'
+		);
 		$res = $dbr->select(
 			'ores_classification',
-			[ 'oresc_id', 'oresc_rev', 'oresc_model', 'oresc_class' ],
+			[ 'oresc_rev', 'oresc_model', 'oresc_class' , 'ids' => $groupConcat ],
 			'',
 			__METHOD__,
 			[ 'GROUP BY' => 'oresc_rev, oresc_model, oresc_class',
 			'HAVING' => 'COUNT(*) > 1' ]
 		);
 		$ids = [];
-		$dump = [];
-		foreach ( $row as $res ) {
-			$key = implode( ',', [ $row->oresc_rev, $row->oresc_model, $row->oresc_class ] );
-			if ( array_has_key( $key, $dump ) ) {
-				$ids[] = $row->oresc_id;
-			} else {
-				$dump[] = $key;
+		foreach ( $res as $row ) {
+			$rowIds = explode( '|', $row->ids );
+			if ( $rowIds > 1 ) { // Sanity
+				$newIds = array_slice( $rowIds, 1 );
+				$ids = array_merge( $ids, $newIds );
 			}
 		}
 		$c = count( $ids );
