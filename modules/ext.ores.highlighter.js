de2f050b249b09ae472495c1eba4fec781fde9b1
Roan Kattouw <roan.kattouw@gmail.com>
2017-05-02 16:16:53 -0700

    Rewrite highlight JS to use mw.Uri and generally be cleaner
    Change-Id: I13899f54ca46d9fa6b74cc1148063ac5223f0853

--- modules/ext.ores.highlighter.js
+++ modules/ext.ores.highlighter.js
@@ -1,30 +1,35 @@
 ( function ( mw, $ ) {
 	'use strict';
-	if ( !$( '.mw-changeslist, .mw-contributions-list' ).length ) {
-		return;
-	}
-	var thresholds = mw.config.get( 'oresThresholds' ).damaging;
-	var names = {};
-	names[thresholds.verylikelybad] = 'verylikelybad';
-	names[thresholds.likelybad] = 'likelybad';
-	names[thresholds.maybebad] = 'maybebad';
-	$( 'li.damaging' ).each( function () {
-		var url = $( this ).children( 'a.mw-changeslist-diff' ).attr( 'href' );
-		if ( !url ) {
-			return true;
+	$( function () {
+		var $changesList = $( '.mw-changeslist, .mw-contributions-list' ),
+			thresholds = mw.config.get( 'oresThresholds' ).damaging,
+			levels = [ 'verylikelybad', 'likelybad', 'maybebad' ],
+			scores = mw.config.get( 'oresData' );
+		if ( !$changesList.length ) {
+			return;
 		}
-		var reg = /\b(?:diff=|diff=prev&oldid=)(\d+)/;
-		var res = reg.exec( url );
-		if ( !res || !( res[1] in mw.config.get( 'oresData' ) ) ) {
-			return true;
-		}
-		var score = mw.config.get( 'oresData' )[res[1]]['damaging'];
-		var threshold = 0;
-		for ( threshold in names ) {
-			if ( score > threshold ) {
-				$( this ).attr( 'data-ores-damaging', names[threshold] );
-				break;
+
+		$( 'li.damaging' ).each( function () {
+			var i, revid, score,
+				$link = $( this ).find( 'a.mw-changeslist-diff' ),
+				href = $link.prop( 'href' ),
+				uri = new mw.Uri( href );
+			if ( !href ) {
+				return;
 			}
-		}
-	} )
+			// URI looks like either diff=prev&oldid=R, or diff=R&oldid=P, and we want R
+			revid = uri.query.diff === 'prev' ? uri.query.oldid : uri.query.diff;
+			if ( !scores[ revid ] ) {
+				return;
+			}
+			score = scores[ revid ].damaging;
+			for ( i = 0; i < levels.length; i++ ) {
+				if ( score > thresholds[ levels[ i ] ] ) {
+					$( this ).attr( 'data-ores-damaging', levels[ i ] );
+					break;
+				}
+			}
+		} );
+	} );
+
 }( mediaWiki, jQuery ) );
