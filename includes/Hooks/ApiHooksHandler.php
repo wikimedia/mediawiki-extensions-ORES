dd949139dd88a5daf8e817a0879b1ea86cfccc03
Kunal Mehta <legoktm@member.fsf.org>
2017-11-20 16:58:24 -0800

    Add missing GPL file headers
    This extension is licensed as GPL v3, or any later version (at your
    option), so add the corresponding GPL file headers.
    
    ApiQueryORES already had a GPL v2 or later file header, I updated it to
    the v3 or later version for consistency with the rest of the extension.
    
    WatchedItemQueryServiceExtension and ApiHooksHandler both had @author
    tags for Brad, so I noted him as the copyright holder for both files.
    
    Change-Id: Ie16893dbbe39fbd67b08f9da3d1e6dd3c69e454c

--- includes/Hooks/ApiHooksHandler.php
+++ includes/Hooks/ApiHooksHandler.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * Copyright (C) 2016 Brad Jorsch <bjorsch@wikimedia.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace ORES\Hooks;
 
@@ -25,9 +41,6 @@ use ResultWrapper;
 use WatchedItem;
 use WatchedItemQueryService;
 
-/**
- * @author Brad Jorsch <bjorsch@wikimedia.org>
- */
 class ApiHooksHandler {
 
 	/**
