0225b6d7295f95d66f95f3550031f8d51f2f7027
petarpetkovic <ppetkovic@wikimedia.org>
2017-12-01 18:39:46 +0100

    Use ORES preference on Related Changes page
    Bug: T180866
    Change-Id: I503c8d1a4f429050eaf1bdfe1fded55b36f199aa

--- includes/Hooks/ChangesListHooksHandler.php
+++ includes/Hooks/ChangesListHooksHandler.php
@@ -49,7 +49,7 @@ class ChangesListHooksHandler {
 		$logFilter = $changeTypeGroup->getFilter( 'hidelog' );
 
 		if ( Hooks::isModelEnabled( 'damaging' ) ) {
-			if ( $clsp instanceof SpecialRecentChanges ) {
+			if ( $clsp instanceof SpecialRecentChanges || $clsp instanceof SpecialRecentChangesLinked ) {
 				$damagingDefault = $clsp->getUser()->getOption( 'oresRCHideNonDamaging' );
 				$highlightDefault = $clsp->getUser()->getBoolOption( 'ores-damaging-flag-rc' );
 			} elseif ( $clsp instanceof SpecialWatchlist ) {
