4ed5c231a35187c766a76e7a7c067feba3aa5292
Amir Sarabadani <ladsgroup@gmail.com>
2017-12-25 18:05:21 +0100

    Split ThresholdLookup into two class
    Bug: T181892
    Change-Id: I23e18d7b56a5184474ee0ec3dea703bd15a54db2

---
+++ tests/phpunit/includes/ThresholdParserTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace ORES\Tests;
+
+use MediaWiki\Logger\LoggerFactory;
+use ORES\ThresholdParser;
+use Psr\Log\LoggerInterface;
+
+/**
+ * @group ORES
+ * @covers ORES\ThresholdParser
+ */
+class ThresholdParserTest extends \MediaWikiTestCase {
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->setMwGlobals( [
+			'wgOresFiltersThresholds' => [
+				'damaging' => [
+					'verylikelygood' => [ 'min' => 0, 'max' => 'recall_at_precision(min_precision=0.98)' ],
+					'maybebad' => false,
+					'likelybad' => [ 'min' => 0.81, 'max' => 1 ],
+					'verylikelybad' => [ 'min' => 'recall_at_precision(min_precision=0.9)', 'max' => 1 ],
+				],
+			],
+		] );
+	}
+
+	private function getLoggerMock() {
+		return $this->getMockBuilder( LoggerInterface::class )
+			->setMethods( [
+				'emergency',
+				'alert',
+				'critical',
+				'error',
+				'warning',
+				'notice',
+				'info',
+				'debug',
+				'log'
+			] )
+			->getMock();
+	}
+
+	/**
+	 * @dataProvider provideTestParseThresholds
+	 */
+	public function testParseThresholds( $expected, $data, $model ) {
+		$thresholdParser = new ThresholdParser( $this->getLoggerMock() );
+		$thresholds = $thresholdParser->parseThresholds( $data, $model );
+		$this->assertSame( $expected, $thresholds );
+	}
+
+	public function provideTestParseThresholds() {
+		$thresholdData1 = [
+			'false' => [ 'maximum recall @ precision >= 0.98' => [ 'threshold' => 0.259 ] ],
+			'true' => [ 'maximum recall @ precision >= 0.9' => [ 'threshold' => 0.945 ] ]
+		];
+		$expected1 = [
+			'verylikelygood' => [ 'min' => 0, 'max' => 0.741 ],
+			'likelybad' => [ 'min' => 0.81, 'max' => 1 ],
+			'verylikelybad' => [ 'min' => 0.945, 'max' => 1 ]
+		];
+
+		$thresholdData2 = [
+			'true' => [ 'maximum recall @ precision >= 0.9' => [ 'threshold' => 0.945 ] ],
+			'false' => []
+		];
+		$expected2 = [
+			'likelybad' => [ 'min' => 0.81, 'max' => 1 ],
+			'verylikelybad' => [ 'min' => 0.945, 'max' => 1 ]
+		];
+
+		return [
+			[ [ 'likelybad' => [ 'min' => 0.81,  'max' => 1 ] ], [], 'damaging' ],
+			[ $expected1, $thresholdData1, 'damaging' ],
+			[ $expected2, $thresholdData2, 'damaging' ],
+		];
+	}
+
+	public function testGetFiltersConfig_oldFiltersConfig() {
+		$thresholdParser = new ThresholdParser( LoggerFactory::getInstance( 'test' ) );
+		$thresholds = $thresholdParser->getFiltersConfig( 'damaging' );
+
+		$this->assertEquals(
+			[
+				'verylikelygood' => [
+					'min' => 0,
+					'max' => 'maximum recall @ precision >= 0.98',
+				],
+				'likelybad' => [
+					'min' => 0.81,
+					'max' => 1,
+				],
+				'verylikelybad' => [
+					'min' => 'maximum recall @ precision >= 0.9',
+					'max' => 1,
+				],
+				'maybebad' => false,
+			],
+			$thresholds
+		);
+	}
+
+	public function testGetFiltersConfig_newFiltersConfig() {
+		$thresholdParser = new ThresholdParser( LoggerFactory::getInstance( 'test' ) );
+		$thresholds = $thresholdParser->getFiltersConfig( 'damaging' );
+
+		$this->assertEquals(
+			[
+				'verylikelygood' => [
+					'min' => 0,
+					'max' => 'maximum recall @ precision >= 0.98',
+				],
+				'likelybad' => [
+					'min' => 0.81,
+					'max' => 1,
+				],
+				'verylikelybad' => [
+					'min' => 'maximum recall @ precision >= 0.9',
+					'max' => 1,
+				],
+				'maybebad' => false,
+			],
+			$thresholds
+		);
+	}
+
+}
