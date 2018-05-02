3dba3c76acd85c1a2d718d02a3e1336fe2aa8a74
Timo Tijhof <krinklemail@gmail.com>
2018-05-03 00:17:43 +0100

    selenium: Use wdio-mediawiki and add standalone runner
    * Update pageobjects/ to use Page of the new 'wdio-mediawiki' package
      from npm, instead of accessing the current MW-branch version
      via ../../tests.
    
    * Add an independant 'npm run selenium-test' entry point
      for local development, and update the README to encourage use
      of this. It comes with a minimal wdio.conf.js file that contains
      only non-default settings, and runs only specs from this extension.
      This also makes it so that screenshots are saved to this repo's
      log directory instead of core's.
    
    Bug: T193088
    Depends-On: I547a7899e7a97693a93567dd763784e637433d55
    Change-Id: I369165a1b0d6967f34961fb9bf8d6705eb5c72c8

--- tests/selenium/pageobjects/recentchanges.page.js
+++ tests/selenium/pageobjects/recentchanges.page.js
@@ -1,12 +1,11 @@
-'use strict';
-const Page = require( '../../../../../tests/selenium/pageobjects/page' );
+const Page = require( 'wdio-mediawiki/Page' );
 
 class RecentChangesPage extends Page {
-
 	get activeFilters() { return browser.element( '.ores-damaging-filter' ); }
 
 	open() {
-		super.open( 'Special:RecentChanges' );
+		super.openTitle( 'Special:RecentChanges' );
 	}
 }
+
 module.exports = new RecentChangesPage();
--- tests/selenium/specs/filters.js
+++ tests/selenium/specs/filters.js
@@ -1,12 +1,9 @@
-'use strict';
-
 var assert = require( 'assert' ),
 	RecentChangesPage = require( '../pageobjects/recentchanges.page' );
 
 describe( 'ORES', function () {
 
 	it( 'filters are present', function () {
-
 		RecentChangesPage.open();
 
 		assert( RecentChangesPage.activeFilters.isExisting() );
