3ea74b100c90656e47f7cbb1d1398eb520f501a2
Ed Sanders <esanders@wikimedia.org>
2018-11-12 16:58:48 +0000

    Remove obsolete aliases from closures
    Bug: T208951
    Change-Id: I756cb68747c5f67e46decc4b4bfa3953cf9d0e82

--- modules/ext.ores.highlighter.js
+++ modules/ext.ores.highlighter.js
@@ -1,34 +1,31 @@
-( function ( mw, $ ) {
+$( function () {
 	'use strict';
 
-	$( function () {
-		var $changesList = $( '.mw-changeslist, .mw-contributions-list' ),
-			thresholds = mw.config.get( 'oresThresholds' ).damaging,
-			// Levels must be descending from the worst to best.
-			levels = [ 'verylikelybad', 'likelybad', 'maybebad' ],
-			scores = mw.config.get( 'oresData' );
-		if ( !$changesList.length ) {
+	var $changesList = $( '.mw-changeslist, .mw-contributions-list' ),
+		thresholds = mw.config.get( 'oresThresholds' ).damaging,
+		// Levels must be descending from the worst to best.
+		levels = [ 'verylikelybad', 'likelybad', 'maybebad' ],
+		scores = mw.config.get( 'oresData' );
+	if ( !$changesList.length ) {
+		return;
+	}
+
+	$( '[data-mw-revid]' ).each( function () {
+		var i, revid, score;
+		revid = $( this ).attr( 'data-mw-revid' );
+		if ( !scores[ revid ] ) {
 			return;
 		}
-
-		$( '[data-mw-revid]' ).each( function () {
-			var i, revid, score;
-			revid = $( this ).attr( 'data-mw-revid' );
-			if ( !scores[ revid ] ) {
-				return;
+		score = scores[ revid ].damaging;
+		for ( i = 0; i < levels.length; i++ ) {
+			if ( score > thresholds[ levels[ i ] ] ) {
+				$( this ).addClass( 'mw-changeslist-damaging-' + levels[ i ] );
+				// $(this) might be collapsed and invisible, in that case highlight the group block as well
+				// we rely on the fact that more severe classes have higher-specificity selectors
+				$( this ).parents( '.mw-enhanced-rc.mw-collapsible' )
+					.addClass( 'damaging mw-changeslist-damaging-' + levels[ i ] );
+				break;
 			}
-			score = scores[ revid ].damaging;
-			for ( i = 0; i < levels.length; i++ ) {
-				if ( score > thresholds[ levels[ i ] ] ) {
-					$( this ).addClass( 'mw-changeslist-damaging-' + levels[ i ] );
-					// $(this) might be collapsed and invisible, in that case highlight the group block as well
-					// we rely on the fact that more severe classes have higher-specificity selectors
-					$( this ).parents( '.mw-enhanced-rc.mw-collapsible' )
-						.addClass( 'damaging mw-changeslist-damaging-' + levels[ i ] );
-					break;
-				}
-			}
-		} );
+		}
 	} );
-
-}( mediaWiki, jQuery ) );
+} );
