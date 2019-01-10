7e214458632a5b29a3c3ec4f25fb967a2f8770ff
Timo Tijhof <krinklemail@gmail.com>
2019-01-09 22:35:14 -0800

    ext.ores.api: Single function
    This removes most of the legacy copied from mw.Api which was
    one of:
    
    - specific to MW (e.g. '?format=json'),
    - specific to needs of UploadWizard,
    - specific to supporting multiple wikis,
    - old quirks we regret, like https://phabricator.wikimedia.org/T176704.
    
    It leaves a short wrapper around $.ajax() that sets a url, a timeout,
    and ensures the data is parsed as JSON before being returned.
    
    It also keeps the more or less random mw.log call we added in core
    for some point for non-aborted request to be logged to the console,
    although we may want to remove this as welll, given most browsers
    now have superior debugging abilities enabled by default.
    
    Change-Id: I40d8cd6caad7aca3e871892d49c6acd59c05ec5e

--- modules/ext.ores.api.js
+++ modules/ext.ores.api.js
@@ -1,117 +1,63 @@
+/**
+ * @class ext.ores.api
+ * @singleton
+ */
 ( function () {
-	/**
-	 * @class OresApi
-	 */
+	'use strict';
+
+	var config = mw.config.get( 'wgExtOresApiConfig' ),
+		url = config.baseUrl + '/v' + config.apiVersion + '/scores/' + config.wikiId;
 
 	/**
-	 * @property {Object} defaultOptions Default options for #ajax calls. Can be overridden by passing
-	 *     `options` to OresApi constructor.
-	 * @property {Object} defaultOptions.parameters Default query parameters for API requests.
-	 * @property {Object} defaultOptions.ajax Default options for jQuery#ajax.
-	 * @private
-	 */
-	var defaultOptions = {
-			parameters: {
-				format: 'json'
-			},
-			ajax: {
-				timeout: 30 * 1000, // 30 seconds
-				dataType: 'json',
-				type: 'GET'
-			}
-		},
-		OresApi;
-
-	/**
-	 * Constructor to create an object to interact with the API of an ORES server.
-	 * OresApi objects represent the API of a particular ORES server.
+	 * Transform any parameters we support non-string shortcuts for
+	 * to their string equivalant.
 	 *
-	 *     var ores = require('ext.ores.api');
+	 * @private
+	 * @param {Object} parameters (modified in-place)
+	 * @return {Object}
+	 */
+	function preprocessParameters( parameters ) {
+		var key;
+		for ( key in parameters ) {
+			if ( Array.isArray( parameters[ key ] ) ) {
+				parameters[ key ] = parameters[ key ].join( '|' );
+			}
+		}
+		return parameters;
+	}
+
+	/**
+	 * Make an API request to an ORES server.
+	 *
+	 *     var ores = require( 'ext.ores.api' );
 	 *     ores.get( {
-	 *         revids: [1234, 1235],
-	 *         models: [ 'damaging', 'articlequality' ] // same effect as 'damaging|articlequality'
-	 *     } ).done( function ( data ) {
+	 *         revids: [ 1234, 1235 ],
+	 *         models: [ 'damaging', 'articlequality' ] // same as 'damaging|articlequality'
+	 *     } ).then( function ( data ) {
 	 *         console.log( data );
 	 *     } );
 	 *
-	 * @constructor
-	 * @param {Object} config configuration of the ores service.
+	 * @param {Object} parameters
+	 * @return {jQuery.Promise} Done: API response data, Fail: Error code.
 	 */
-	OresApi = function ( config ) {
-		var defaults = {};
+	function get( parameters ) {
+		var req = $.ajax( {
+			url: url,
+			data: preprocessParameters( parameters ),
+			timeout: 30 * 1000, // 30 seconds
+			dataType: 'json'
+		} );
 
-		defaults.parameters = $.extend( {}, defaultOptions.parameters, defaults.parameters );
-		defaults.ajax = $.extend( {}, defaultOptions.ajax, defaults.ajax );
-		defaults.ajax.url = config.baseUrl + '/v' + config.apiVersion + '/scores/' + config.wikiId;
+		req.fail( function ( code, details ) {
+			if ( !( code === 'http' && details && details.textStatus === 'abort' ) ) {
+				mw.log.warn( 'ORES API error:', code, details );
 
-		this.defaults = defaults;
-		this.requests = [];
-	};
-
-	OresApi.prototype = {
-		/**
-		 * Massage parameters from the nice format we accept into a format suitable for the API.
-		 *
-		 * @private
-		 * @param {Object} parameters (modified in-place)
-		 */
-		preprocessParameters: function ( parameters ) {
-			var key;
-			for ( key in parameters ) {
-				if ( Array.isArray( parameters[ key ] ) ) {
-					parameters[ key ] = parameters[ key ].join( '|' );
-				} else if ( parameters[ key ] === false || parameters[ key ] === undefined ) {
-					// Boolean values are only false when not given at all
-					delete parameters[ key ];
-				}
 			}
-		},
+		} );
 
-		/**
-		 * Perform the API call.
-		 *
-		 * @param {Object} parameters
-		 * @return {jQuery.Promise} Done: API response data and the jqXHR object.
-		 *  Fail: Error code
-		 */
-		get: function ( parameters ) {
-			var requestIndex,
-				api = this,
-				apiDeferred = $.Deferred(),
-				xhr,
-				ajaxOptions;
+		return req;
+	}
 
-			parameters = $.extend( {}, this.defaults.parameters, parameters );
-			ajaxOptions = $.extend( {}, this.defaults.ajax );
-
-			this.preprocessParameters( parameters );
-
-			ajaxOptions.data = $.param( parameters );
-
-			xhr = $.ajax( ajaxOptions )
-				.done( function ( result, textStatus, jqXHR ) {
-					var code;
-					if ( result.error ) {
-						code = result.error.code === undefined ? 'unknown' : result.error.code;
-						apiDeferred.reject( code, result, result, jqXHR );
-					} else {
-						apiDeferred.resolve( result, jqXHR );
-					}
-				} );
-
-			requestIndex = this.requests.length;
-			this.requests.push( xhr );
-			xhr.always( function () {
-				api.requests[ requestIndex ] = null;
-			} );
-			return apiDeferred.promise( { abort: xhr.abort } ).fail( function ( code, details ) {
-				if ( !( code === 'http' && details && details.textStatus === 'abort' ) ) {
-					mw.log( 'OresApi error: ', code, details );
-				}
-			} );
-		}
-	};
-
-	module.exports = new OresApi( {}, mw.config.get( 'wgExtOresApiConfig' ) );
+	module.exports = { get: get };
 
 }() );
